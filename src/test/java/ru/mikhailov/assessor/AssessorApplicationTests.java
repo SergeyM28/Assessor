d83ef7f33a4d685e494b50d00ca4a5ea68967964
SergeiM <mihailovsv@mail.ru>
2024-04-09 21:11:36 +0300

    Initial commit

---
+++ src/test/java/ru/mikhailov/assessor/AssessorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikhailov.assessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
